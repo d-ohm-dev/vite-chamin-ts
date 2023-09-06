41555f35c35a61935da2d879d85a376495e9b5e3
D-Ohm <demian07@gmail.com>
2023-09-06 19:38:29 -0300

    optimizando Product View

--- src/containers/ProductView.tsx
+++ src/containers/ProductView.tsx
@@ -1,21 +1,32 @@
-import { Grid, GridItem } from "@chakra-ui/react";
-import PlacementExample from "../elements/PV/rollNav";
+import { Box, Grid, GridItem, useColorModeValue } from "@chakra-ui/react";
+import Navbar from "../elements/PV/NavbarPV";
+import { ColorModeSwitcher } from "../elements/FP/ColorModeSwitcher";
+import FlexSchema from "../elements/PV/FlexSchema";
+// import PlacementExample from "../elements/PV/rollNav";
 
 
 const ProductView = () => (
-  <Grid
-  h='200px'
-  templateRows='repeat(2, 1fr)'
-  templateColumns='repeat(5, 1fr)'
-  gap={4}
->
-  <GridItem rowSpan={2} colSpan={1} bg='tomato'>
-    <PlacementExample/>
-  </GridItem>
-  <GridItem colSpan={2} bg='papayawhip' />
-  <GridItem colSpan={2} bg='papayawhip' />
-  <GridItem colSpan={4} bg='tomato' />
-</Grid>
+  // textAlign="center" fontSize="xl"
+  <Box textAlign="center" fontSize="xl" >
+    <Grid
+    h='8xl'
+    templateRows='repeat(4, 1fr, auto)'
+    templateColumns='repeat(1, 100%)'
+    gap={0}
+    bg={useColorModeValue('linear-gradient( to bottom right, rgba(244, 244, 244, 1), rgb(137, 209, 253) )', 'linear-gradient( to bottom right, #0c4083, rgba(19, 29, 77, 0.94) )')}
+    >
+    <GridItem rowSpan={1} colSpan={1}>
+    <ColorModeSwitcher display='flex' position='relative' top='15%' left='93%' />
+      {/* <PlacementExample/> */}
+      <Navbar/>
+    </GridItem>
+    <GridItem rowSpan={2} colSpan={1} >
+       <FlexSchema />
+    </GridItem>
+    <GridItem rowSpan={3} colSpan={1} bg='papayawhip' />
+    <GridItem rowSpan={4} colSpan={1} bg= 'aquamarine' />
+  </Grid>
+</Box>
 )
 
 export default ProductView
--- src/elements/PV/rollNav.tsx
+++ src/elements/PV/rollNav.tsx
@@ -1,16 +1,28 @@
-import { Box, Button, Drawer, DrawerBody, DrawerContent, DrawerHeader, DrawerOverlay, Radio, RadioGroup, SlideDirection, Stack, useDisclosure } from "@chakra-ui/react"
-import { useState } from "react"
+import { 
+  Box, 
+  Button, 
+  Drawer, 
+  DrawerBody, 
+  DrawerContent, 
+  DrawerHeader, 
+  DrawerOverlay, 
+  // Radio, 
+  // RadioGroup, 
+  // SlideDirection, 
+  // Stack, 
+  useDisclosure } from "@chakra-ui/react"
+
 
 
 function PlacementExample() {
   const { isOpen, onOpen, onClose } = useDisclosure()
-  const [placement, setPlacement] = useState<SlideDirection>("right")
-
+  // const { isOpen, onClose } = useDisclosure()
+  // const [placement, setPlacement] = useState<SlideDirection>("right")
 
 
   return (
-    <Box>
-      <RadioGroup defaultValue={placement} onChange={(val: SlideDirection) => setPlacement(val)}>
+    <Box >
+      {/* <RadioGroup defaultValue={placement} onChange={(val: SlideDirection) => setPlacement(val)}>
         <Stack direction='row' mb='4'>
           <Radio value="top">Top</Radio>
           <Radio value="right">Right</Radio>
@@ -20,8 +32,12 @@ function PlacementExample() {
       </RadioGroup>
       <Button colorScheme='blue' onClick={onOpen}>
         Open
+      </Button> */}
+      {/* <Drawer placement={placement} onClose={onClose} isOpen={isOpen}> */}
+      <Button colorScheme='blue' onClick={onOpen} mt={450}>
+        Open
       </Button>
-      <Drawer placement={placement} onClose={onClose} isOpen={isOpen}>
+      <Drawer placement={'left'} onClose={onClose} isOpen={isOpen}>
         <DrawerOverlay />
         <DrawerContent>
           <DrawerHeader borderBottomWidth='1px'>Basic Drawer</DrawerHeader>
