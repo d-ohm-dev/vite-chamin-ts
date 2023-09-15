18f92cb41a73e0ba6b579c768c5d1e32e7d75174
D-Ohm <demian07@gmail.com>
2023-09-14 21:38:13 -0300

    agrego gallery y details

--- src/containers/ProductViewGallery.tsx
+++ src/containers/ProductViewGallery.tsx
@@ -13,24 +13,24 @@ const ProductViewGallery = () => (
   <Box textAlign="center" fontSize="xl" >
     <Grid
     h='8xl'
-    templateRows='repeat(4, 1fr, auto)'
+    templateRows='repeat(3, 1fr, auto)'
     templateColumns='repeat(1, 100%)'
     gap={0}
     bg={useColorModeValue('linear-gradient( to bottom right, rgba(244, 244, 244, 1), rgb(137, 209, 253) )', 'linear-gradient( to bottom right, #0c4083, rgba(19, 29, 77, 0.94) )')}
     >
-    <GridItem rowSpan={1} colSpan={1}>
-    <ColorModeSwitcher display='flex' position='relative' top='15%' left='93%' />
-      {/* <PlacementExample/> */}
-      <Navbar/>
-    </GridItem>
-    <GridItem rowSpan={2} colSpan={1} >
-      <Gallery/>
-    </GridItem>
-    <GridItem rowSpan={3} colSpan={1}>
-      <Footer/>
-    </GridItem>
-  </Grid>
-</Box>
+      <GridItem rowSpan={1} colSpan={1}>
+        <ColorModeSwitcher display='flex' position='relative' top='15%' left='93%' />
+        {/* <PlacementExample/> */}
+        <Navbar/>
+      </GridItem>
+      <GridItem rowSpan={2} colSpan={1} >
+        <Gallery/>
+      </GridItem>
+      <GridItem rowSpan={3} colSpan={1} mt={'96'}>
+        <Footer/>
+      </GridItem>
+    </Grid>
+  </Box>
 )
 
 export default ProductViewGallery
