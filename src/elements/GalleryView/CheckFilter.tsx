2173d5908c8f64db65a6c984b83e19bdf67430b3
D-Ohm <demian07@gmail.com>
2023-09-18 22:47:54 -0300

    mejoras en productGallery

--- src/elements/GalleryView/CheckFilter.tsx
+++ src/elements/GalleryView/CheckFilter.tsx
@@ -1,10 +1,15 @@
 
 
 import React from "react"
+<<<<<<< HEAD
 import { Checkbox, 
   Stack, 
   useColorModeValue 
 } from "@chakra-ui/react"
+=======
+import { Checkbox, Stack } from "@chakra-ui/react"
+
+>>>>>>> af8adc3... mojoras ui filtro
 
 
 
@@ -13,7 +18,12 @@ export default function CheckFilter() {
 
   const allChecked = checkedItems.every(Boolean)
   const isIndeterminate = checkedItems.some(Boolean) && !allChecked
+<<<<<<< HEAD
   const colorMode1 = useColorModeValue( 'SlateGray', 'WhiteSmoke')
+=======
+  // const colorMode1 = useColorModeValue('gray.800', 'white')
+
+>>>>>>> af8adc3... mojoras ui filtro
 
 
   return (
@@ -22,8 +32,15 @@ export default function CheckFilter() {
         isChecked={allChecked}
         isIndeterminate={isIndeterminate}
         onChange={(e) => setCheckedItems([e.target.checked, e.target.checked])}
+<<<<<<< HEAD
         border={colorMode1}
         colorScheme="red"
+=======
+
+        // color={colorMode1}
+        // iconColor={colorMode1}
+
+>>>>>>> af8adc3... mojoras ui filtro
       >
         Parent Checkbox
       </Checkbox>
@@ -31,16 +48,28 @@ export default function CheckFilter() {
         <Checkbox
           isChecked={checkedItems[0]}
           onChange={(e) => setCheckedItems([e.target.checked, checkedItems[1]])}
+<<<<<<< HEAD
           border={colorMode1}
           colorScheme="red"
+=======
+          // colorScheme={colorMode1}
+          // color={colorMode1}
+          // iconColor={colorMode1}
+>>>>>>> af8adc3... mojoras ui filtro
         >
           Child Checkbox 1
         </Checkbox>
         <Checkbox
           isChecked={checkedItems[1]}
           onChange={(e) => setCheckedItems([checkedItems[0], e.target.checked])}
+<<<<<<< HEAD
           border={colorMode1}
           colorScheme="red"
+=======
+          // colorScheme={colorMode1}
+          // color={colorMode1}
+          // iconColor={colorMode1}
+>>>>>>> af8adc3... mojoras ui filtro
         >
           Child Checkbox 2
         </Checkbox>
