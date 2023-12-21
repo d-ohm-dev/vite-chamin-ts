ed50306aa7815cb533f59b493600581243999886
D-Ohm <demian07@gmail.com>
2023-12-20 23:10:15 -0300

    newNavBar

--- src/containers/ProductViewGallery.tsx
+++ src/containers/ProductViewGallery.tsx
@@ -1,7 +1,6 @@
 import { Box, Grid, GridItem, useColorModeValue } from "@chakra-ui/react";
 // import Navbar from "../elements/ProductView/NavbarPV";
 import Navbar from "../elements/CommonElements/newNavBar";
-import ColorModeSwitcher from "../elements/FrontPage/ColorModeSwitcher";
 // import FlexView from "../elements/ProductView/ProductDetails";
 import Footer from "../elements/CommonElements/Footer";
 import Gallery from "../elements/GalleryView/ProductGallery";
@@ -20,7 +19,6 @@ const ProductViewGallery = () =>{
     bg={useColorModeValue('linear-gradient( to bottom right, rgba(244, 244, 244, 1), rgb(137, 209, 253) )', 'linear-gradient( to bottom right, #0c4083, rgba(19, 29, 77, 0.94) )')}
     >
       <GridItem rowSpan={1} colSpan={1}>
-        <ColorModeSwitcher display='flex' position='relative' top='15%' left='93%' autoFocus={true} />
         {/* <PlacementExample/> */}
         <Navbar />
       </GridItem>
