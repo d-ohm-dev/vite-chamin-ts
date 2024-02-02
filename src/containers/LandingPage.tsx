fc8ab90bb45795ef199b5225f3da9680cf58fe38
D-Ohm <demian07@gmail.com>
2024-02-02 20:07:19 -0300

    mejoras en responsive navbar

--- src/containers/LandingPage.tsx
+++ src/containers/LandingPage.tsx
@@ -18,7 +18,7 @@ const LandingPage = () => {
     bg={useColorModeValue('linear-gradient( to bottom right, rgba(244, 244, 244, 1), rgb(137, 209, 253) )', 'linear-gradient( to bottom right, #0c4083, rgba(19, 29, 77, 0.94) )')}
     >
       <GridItem rowSpan={1} colSpan={1} >
-        <Navbar height={'40'}/>
+        <Navbar/>
       </GridItem>
 
 
